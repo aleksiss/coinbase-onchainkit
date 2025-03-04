8fb0b8691325d707a58d1004e5f1a484516a580d
aleksiss <alextsig@gmail.com>
2025-03-04 09:03:46 +0200

    Update constants.ts

--- src/buy/constants.ts
+++ src/buy/constants.ts
@@ -7,12 +7,6 @@ export const ONRAMP_PAYMENT_METHODS = [
   },
   
   {
-    id: 'APPLE_PAY',
-    name: 'Apple Pay',
-    description: 'Up to $500/week',
-    icon: 'applePay',
-  },
-  {
     id: 'CARD',
     name: 'Debit Card',
     description: 'Up to $500/week',
